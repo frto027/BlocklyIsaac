38a2c569f2aa09fea64086ca3cd9968afef44ad3
frto027 <602706150@qq.com>
2020-11-07 11:10:46 +0800

    copy/paste block to clipboard

--- electron/inject.js
+++ electron/inject.js
@@ -10,7 +10,7 @@
 
     //we are running in electron, do more things
 
-    const { shell, ipcRenderer } = require('electron')
+    const { shell, ipcRenderer, clipboard } = require('electron')
     //用户点击帮助时打开浏览器，而不是新窗口
     //redefine the show help url to open a real web browser
     Blockly.BlockSvg.prototype.showHelp =function(){
@@ -28,8 +28,45 @@
         old_change_language()
     }
 
+    //出于安全考虑，我们不加载第三方js脚本
+    Code.importPrettify = function(){}
     //给复制粘贴打补丁，当复制粘贴块时，应该复制到系统剪切板
-    //TODO
+    CLIPBOARD_PREFIX = 'bcdt'
+    CLIPBOARD_TAIL = 'isend'
+    //TODO: 剪切板压缩存储
+    Blockly.__defineGetter__('clipboardXml_', function(){
+        var text = clipboard.readText()
+        try{
+            text = Buffer.from(text,'base64').toString('utf-8')
+            if(text.startsWith(CLIPBOARD_PREFIX) && text.endsWith(CLIPBOARD_TAIL)){
+                text = text.substr(CLIPBOARD_PREFIX.length,text.length - CLIPBOARD_PREFIX.length - CLIPBOARD_TAIL.length)
+                return Blockly.Xml.textToDom(text)
+            }
+        }catch(e){ }
+        
+        return undefined
+    })
+    Blockly.__defineSetter__('clipboardXml_',function(clipboardXml){
+        var text = Blockly.Xml.domToText(clipboardXml)
+        text = CLIPBOARD_PREFIX + text + CLIPBOARD_TAIL
+        text = Buffer.from(text,'utf-8').toString('base64')
+        clipboard.writeText(text)
+    })
+    //需要拦截原版的ctrl+v
+    let orig_onKyeDown = Blockly.onKeyDown
+    Blockly.onKeyDown = function(e){
+        if (e.altKey || e.ctrlKey || e.metaKey) {
+            if (e.keyCode == Blockly.utils.KeyCodes.V) {
+                if(Blockly.clipboardXml_ != undefined){
+                    Code.workspace.paste(Blockly.clipboardXml_)
+                }
+
+                return;
+            }
+        }
+
+        orig_onKyeDown(e)
+    }
 
     //增加文件保存按钮
 
