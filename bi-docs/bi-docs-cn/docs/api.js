4c062676914c079c6f45a70b5644a2385010b0a4
frto027 <602706150@qq.com>
2021-09-20 18:29:48 +0800

    doc bug fixed

--- bi-docs/bi-docs-cn/docs/api.js
+++ bi-docs/bi-docs-cn/docs/api.js
@@ -2,9 +2,9 @@ let blockly_isaac_workspace_url = undefined
 
 //我们把编辑器的url写在这里
 if(window.location.href.indexOf('gitee.io') != -1){
-    blockly_isaac_workspace_url = window.location.protocol + "//frto027.gitee.io/BlocklyIsaac/"
+    blockly_isaac_workspace_url = window.location.protocol + "//frto027.gitee.io/blocklyisaac/"
 }else if(window.location.href.indexOf('github.io') != -1){
-    blockly_isaac_workspace_url = window.location.protocol + "//frto027.github.io/blocklyisaac/"
+    blockly_isaac_workspace_url = window.location.protocol + "//frto027.github.io/BlocklyIsaac/"
 }else{
     blockly_isaac_workspace_url = window.location.origin + "/"
 }
@@ -30,6 +30,4 @@ for(let span of document.getElementsByTagName('span')){
     a.innerText = "下载工程"
     a.href= blockly_isaac_workspace_url + url
     span.appendChild(a)
-
-    span.appendChild("<br>")
 }
